65726b0256bf5ee71a7ba9387135659f5c6e9bee
Tiutiunnyk-Liliia <yalili4ka1210@gmail.com>
2020-08-21 09:49:30 +0300

    Developing section "product" is done

--- js/main.js
+++ js/main.js
@@ -14,4 +14,17 @@ $(function(){
         $(this).addClass('tab--active');
         $($(this).attr('href')).addClass('tabs-content--active');
     });
+
+    $('.product-item__favorite').on('click',function () {
+        $(this).toggleClass('product-item__favorite--active')
+    })
+
+    $('.product-slider').slick({
+        slidesToScroll: 1,
+        slidesToShow: 4,
+        prevArrow: '<button class="product-slider__slider-btn product-slider__slider-btnprev">' +
+            '<img src="images/arrow-black-left.svg" alt=""></button>',
+        nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnnext">' +
+            '<img src="images/arrow-black-right.svg" alt=""></button>',
+    });
 });
