89cfcd441a511e5ee0c71a57e6f54b072ce69fee
Tiutiunnyk-Liliia <yalili4ka1210@gmail.com>
2020-09-05 17:52:28 +0300

    Developing section "product" media is done

--- js/main.js
+++ js/main.js
@@ -48,6 +48,39 @@ $(function(){
             '<img src="images/arrow-black-left.svg" alt=""></button>',
         nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnnext">' +
             '<img src="images/arrow-black-right.svg" alt=""></button>',
+        responsive: [
+            {
+                breakpoint: 1301,
+                settings: {
+                    arrows: false,
+                    dots: true,
+                }
+            },
+            {
+                breakpoint: 1201,
+                settings: {
+                    slidesToShow: 3,
+                    arrows: false,
+                    dots: true,
+                }
+            },
+            {
+                breakpoint: 870,
+                settings: {
+                    slidesToShow: 2,
+                    arrows: false,
+                    dots: true,
+                }
+            },
+            {
+                breakpoint: 590,
+                settings: {
+                    slidesToShow: 1,
+                    arrows: false,
+                    dots: true,
+                }
+            }
+        ]
     });
 
     $('.filter-style').styler();
